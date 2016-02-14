a2e1bba7cadc83dbf269eb676be5e348b8cbd62c
A. Gary Bardet <agbardet@gmail.com>
2016-02-13 23:20:05 -0500

    Add Thymeleaf tag to the html files

---
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+	@RequestMapping("/product")
+	public String getProduct() {
+		return "product";
+	}
+}
